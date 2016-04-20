0ed960dfb67a1b0fa20799a1f7be95a0664c476c
Martin Knoll <martinknoll@me.com>
2016-04-20 11:42:35 +0200

    libftdi working

--- Src/ADC_bone.c
+++ Src/ADC_bone.c
@@ -13,43 +13,56 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
-#include <libserialport.h>
+#include <signal.h>
+#include <libftdi1/ftdi.h>
 
 FILE * logfile;
-struct sigaction old_action;
 int total_bytes = 0;
 extern int errno;
 
-void int_handler(int sig)
+static int exit_requested = 0;
+static void sigint_handler(int signum)
 {
-	printf("\nflushing buffers...\n");
-	fflush(stdout);
-	fflush(logfile);
-	exit(0);
+    exit_requested = 1;
 }
 
 int main(int argc, char * argv[])
 {
 
-	char buffer[1024];
-	struct sp_port * port;
+	struct ftdi_context *ftdi;
+	uint8_t buffer[1024];
 
-	sp_get_port_by_name(argv[1], &port);
-	if (sp_open(port,SP_MODE_READ) != SP_OK) {
-		fprintf(stderr,"Error opening Port\n");
-		return EXIT_FAILURE;
-	}
-	struct sp_port_config * config;
-	sp_new_config(&config);
-	sp_set_config_baudrate(config,3000000);
-	sp_set_config_bits(config,8);
-	sp_set_config_parity(config,SP_PARITY_NONE);
-	sp_set_config_stopbits(config,1);
-	if (sp_set_config(port,config) != SP_OK) {
-		fprintf(stderr,"Error configuring Port\n");
-		return EXIT_FAILURE;
-	}
-	sp_free_config(config);
+	signal(SIGINT, sigint_handler);
+
+    if ((ftdi = ftdi_new()) == 0)
+    {
+        fprintf(stderr, "ftdi_new failed\n");
+        return EXIT_FAILURE;
+    }
+
+    ftdi_set_interface(ftdi, 1);
+
+	int f = 0;
+    f = ftdi_usb_open(ftdi, 0x403, 0x6010);
+    if (f < 0)
+    {
+    	fprintf(stderr, "unable to open ftdi device: %d (%s)\n", f, ftdi_get_error_string(ftdi));
+    	exit(-1);
+    }
+
+    f = ftdi_set_baudrate(ftdi, 8000000);
+    if (f < 0)
+    {
+        fprintf(stderr, "unable to set baudrate: %d (%s)\n", f, ftdi_get_error_string(ftdi));
+        exit(-1);
+    }
+
+    f = ftdi_set_line_property(ftdi, 8, STOP_BIT_1, NONE);
+    if (f < 0)
+    {
+        fprintf(stderr, "unable to set line parameters: %d (%s)\n", f, ftdi_get_error_string(ftdi));
+        exit(-1);
+    }
 
 	logfile = fopen("logfile","w");
 
@@ -60,19 +73,20 @@ int main(int argc, char * argv[])
 		return EXIT_FAILURE;
 	}
 
-	while (1) {
-		int count = sp_blocking_read(port,buffer,8,100);
+	while (!exit_requested) {
+		int count = ftdi_read_data(ftdi, buffer, sizeof(buffer));;
 		fwrite(buffer, sizeof(char), count, logfile);
 		total_bytes += count;
-		if (!(total_bytes & 0xFF)) {
-			printf("\rTotal bytes: %d",total_bytes);
-			fflush(stdout);
-		}
+		printf("\rTotal bytes: %d",total_bytes);
+		fflush(stdout);
 	}
-
+	printf("\n\rCleaning up...\n\r");
+	fflush(logfile);
 	fclose(logfile);
-	sp_close(port);
-	sp_free_port(port);
+    signal(SIGINT, SIG_DFL);
+
+    ftdi_usb_close(ftdi);
+    ftdi_free(ftdi);
 
 	return EXIT_SUCCESS;
 }
