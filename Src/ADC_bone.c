864c064cf2fbad43e7edadedfd4f5f465318ad01
Martin Knoll <martinknoll@me.com>
2016-04-20 18:42:26 +0200

    logfile per channel

--- Src/ADC_bone.c
+++ Src/ADC_bone.c
@@ -16,7 +16,8 @@
 #include <signal.h>
 #include <libftdi1/ftdi.h>
 
-FILE * logfile;
+FILE * files[4] = {0};
+char * filenames[4] = {"channel1.bin", "channel2.bin", "channel3.bin", "channel4.bin"};
 int total_bytes = 0;
 extern int errno;
 
@@ -30,7 +31,6 @@ int main(int argc, char * argv[])
 {
 
 	struct ftdi_context *ftdi;
-	uint8_t buffer[1024];
 
 	signal(SIGINT, sigint_handler);
 
@@ -64,25 +64,73 @@ int main(int argc, char * argv[])
         exit(-1);
     }
 
-	logfile = fopen("logfile","w");
+    for (int i=0; i<4; i++) {
+    	files[i] = fopen(filenames[i],"w");
+    	if (files[i] == NULL) {
+    		fprintf(stderr, "Value of errno: %d\n", errno);
+    		perror("Error printed by perror");
+    		fprintf(stderr, "Error opening file: %s\n", strerror(errno));
+    		return EXIT_FAILURE;
+    	}
+    }
 
-	if (logfile == NULL) {
-		fprintf(stderr, "Value of errno: %d\n", errno);
-		perror("Error printed by perror");
-		fprintf(stderr, "Error opening file: %s\n", strerror(errno));
-		return EXIT_FAILURE;
-	}
+	enum decoder_state {
+		IDLE,
+		PREAMBLE,
+		CHANNEL,
+		DATA
+	};
 
+	enum decoder_state state = IDLE;
+	uint16_t active_channel = 1;
+	uint16_t samples = 0;
+	uint8_t buffer[1024];
+	uint16_t count = 0;
 	while (!exit_requested) {
-		int count = ftdi_read_data(ftdi, buffer, sizeof(buffer));;
-		fwrite(buffer, sizeof(char), count, logfile);
-		total_bytes += count;
+		switch (state) {
+			case IDLE:
+				count = ftdi_read_data(ftdi, buffer, 1);
+				if (count && (0xFF == buffer[0])) {
+					state = PREAMBLE;
+				}
+				break;
+			case PREAMBLE:
+				count = ftdi_read_data(ftdi, buffer, 1);
+				if (count && (0xFF == buffer[0])) {
+					state = CHANNEL;
+				}
+				if (count && (0xFF != buffer[0])) {
+					state = IDLE;
+				}
+				break;
+			case CHANNEL:
+				count = ftdi_read_data(ftdi, buffer, 2);
+				if (count) {
+					active_channel = (uint16_t)buffer[0];
+					state = DATA;
+				}
+				break;
+			case DATA:
+				count = ftdi_read_data(ftdi, buffer, 1000-samples);
+				samples += count;
+				total_bytes += count;
+				if (1000 == samples) {
+					samples = 0;
+					state = IDLE;
+					fwrite(buffer, sizeof(char), count, files[active_channel-1]);
+				}
+				break;
+		}
 		printf("\rTotal bytes: %d",total_bytes);
 		fflush(stdout);
 	}
 	printf("\n\rCleaning up...\n\r");
-	fflush(logfile);
-	fclose(logfile);
+
+	for (int i=0; i<4; i++) {
+		fflush(files[i]);
+		fclose(files[i]);
+	}
+
     signal(SIGINT, SIG_DFL);
 
     ftdi_usb_close(ftdi);
