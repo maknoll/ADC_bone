fc2c6735a0350ebf693c3b34e7cce9f8a8d4f20e
Martin Knoll <martinknoll@me.com>
2016-04-26 14:50:53 +0200

    Bugfixing and errorhandling

--- Src/ADC_bone.c
+++ Src/ADC_bone.c
@@ -17,7 +17,6 @@
 #include <libftdi1/ftdi.h>
 
 FILE * files[4] = {0};
-char * filenames[4] = {"channel1.bin", "channel2.bin", "channel3.bin", "channel4.bin"};
 int total_bytes = 0;
 extern int errno;
 
@@ -29,6 +28,26 @@ static void sigint_handler(int signum)
 
 int main(int argc, char * argv[])
 {
+	uint32_t baudrate = 8000000;
+	char filenameprefix[64] = "channel";
+    int vid = 0x403;
+    int pid = 0x6010;
+
+	int optint;
+    while ((optint = getopt(argc, argv, "b:f::")) != -1)
+    {
+        switch (optint)
+        {
+        case 'b':
+        	baudrate = strtoul(optarg, NULL, 0);
+        	break;
+        case 'f':
+        	snprintf(filenameprefix,sizeof(filenameprefix),"%s",optarg);
+        default:
+            fprintf(stderr, "usage: %s [-b baudrate] [-f filename-prefix]\n", *argv);
+            exit(-1);
+        }
+    }
 
 	struct ftdi_context *ftdi;
 
@@ -43,7 +62,7 @@ int main(int argc, char * argv[])
     ftdi_set_interface(ftdi, 1);
 
 	int f = 0;
-    f = ftdi_usb_open(ftdi, 0x403, 0x6010);
+    f = ftdi_usb_open(ftdi, vid, pid);
     if (f < 0)
     {
     	fprintf(stderr, "unable to open ftdi device: %d (%s)\n", f, ftdi_get_error_string(ftdi));
@@ -65,7 +84,9 @@ int main(int argc, char * argv[])
     }
 
     for (int i=0; i<4; i++) {
-    	files[i] = fopen(filenames[i],"w");
+    	char filename[128];
+    	snprintf(filename, sizeof(filename), "%s-%i.bin", filenameprefix, i);
+    	files[i] = fopen(filename,"w");
     	if (files[i] == NULL) {
     		fprintf(stderr, "Value of errno: %d\n", errno);
     		perror("Error printed by perror");
@@ -84,7 +105,7 @@ int main(int argc, char * argv[])
 	enum decoder_state state = IDLE;
 	uint16_t active_channel = 1;
 	uint16_t samples = 0;
-	uint8_t buffer[1024];
+	uint8_t buffer[2000];
 	uint16_t count = 0;
 	while (!exit_requested) {
 		switch (state) {
@@ -107,17 +128,21 @@ int main(int argc, char * argv[])
 				count = ftdi_read_data(ftdi, buffer, 2);
 				if (count) {
 					active_channel = (uint16_t)buffer[0];
-					state = DATA;
+					if ((0 < active_channel) && (active_channel <= 4)) {
+						state = DATA;
+					} else {
+						state = IDLE;
+					}
 				}
 				break;
 			case DATA:
-				count = ftdi_read_data(ftdi, buffer, 1000-samples);
+				count = ftdi_read_data(ftdi, buffer, sizeof(buffer)-samples);
 				samples += count;
 				total_bytes += count;
-				if (1000 == samples) {
+				if (2000 <= samples) {
 					samples = 0;
 					state = IDLE;
-					fwrite(buffer, sizeof(char), count, files[active_channel-1]);
+					fwrite(buffer, sizeof(char), sizeof(buffer), files[active_channel-1]);
 				}
 				break;
 		}
